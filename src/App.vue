193d03460cdfffb68e4ed33067de185ea6ab6769
DariaDesiderata <daria.calwell@gmail.com>
2017-05-08 13:45:55 -0600

    add logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/IBM_Bluemix_logo.svg.png">
     <router-view></router-view>
   </div>
 </template>
@@ -19,5 +19,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img {
+  height: 300px;
+  width: auto;
 }
 </style>
