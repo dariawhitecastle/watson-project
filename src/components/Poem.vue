70e93226dca5ecb54043c26f525764c75adea966
DariaDesiderata <daria.calwell@gmail.com>
2017-05-08 21:58:13 -0600

    add cards to poem component

--- src/components/Poem.vue
+++ src/components/Poem.vue
@@ -6,7 +6,27 @@
       <li>Start over</li>
     </ul>
     <div class="mdl-grid">
-    </div>  
+      <div class="mdl-card mdl-shadow--4dp mdl-cell--5-col mdl-cell--6-col-tablet mdl-cell--12-col-phone">
+        <div class="mdl-card__title">
+           <h2 class="mdl-card__title-text">Poem Title<br>Poem Author</h2>
+        </div>
+        <div class="mdl-card__supporting-text">
+        The Sky Tower is an observation and telecommunications tower located in Auckland,
+        New Zealand. It is 328 metres (1,076 ft) tall, making it the tallest man-made structure
+        in the Southern Hemisphere.
+        </div>
+      </div>
+      <div class="mdl-card mdl-shadow--4dp mdl-cell--5-col mdl-cell--6-col-tablet mdl-cell--12-col-phone">
+        <div class="mdl-card__title">
+           <h2 class="mdl-card__title-text">Poem Title<br>Poem Author</h2>
+        </div>
+        <div class="mdl-card__supporting-text">
+        The Sky Tower is an observation and telecommunications tower located in Auckland,
+        New Zealand. It is 328 metres (1,076 ft) tall, making it the tallest man-made structure
+        in the Southern Hemisphere.
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,6 +47,9 @@ h1, h2 {
   font-weight: normal;
 }
 
+.mdl-card {
+  margin: 20px;
+}
 ul {
   list-style-type: none;
   padding: 0;
